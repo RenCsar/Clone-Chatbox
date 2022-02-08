d1e08753bdde2f5307e2f5ec810981454feab520
Renan Cesar <cesar.renan@hotmail.com>
2022-02-07 23:52:51 -0300

    Adicionando botão para fechar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-let bubble = document.querySelector('.bubble');
-let exit = document.querySelector('.exit');
+let bubble = document.querySelector('.container-bubble');
+let balao = document.querySelector('.bubble');
+let x = document.querySelector('.x-close');
 let livechat = document.querySelector('.livechat');
 
 bubble.addEventListener('click', () => {
-  //livechat.classList.add('animate-chat');
   livechat.classList.toggle("animate-chat");
+  balao.classList.toggle("sumir-balao");
+  x.classList.toggle("chat-fechado");
 });
