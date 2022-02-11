f3781c74a2b8ef01b89f6f8823fb08995cf6eb4f
Renan Cesar <cesar.renan@hotmail.com>
2022-02-11 19:28:10 -0300

    formatação do código

--- js/main.js
+++ js/main.js
@@ -1,25 +1,23 @@
-let bubble = document.querySelector('.container-bubble');
-let balao = document.querySelector('.bubble');
-let x = document.querySelector('.x-close');
-let livechat = document.querySelector('.livechat');
-let send = document.querySelector('.enviar-container');
-let texting = document.querySelector('.texting');
-let clip =document.querySelector('#clip-container');
-
+let bubble = document.querySelector('.container-bubble')
+let balao = document.querySelector('.bubble')
+let x = document.querySelector('.x-close')
+let livechat = document.querySelector('.livechat')
+let send = document.querySelector('.enviar-container')
+let texting = document.querySelector('.texting')
+let clip = document.querySelector('#clip-container')
 
 bubble.addEventListener('click', () => {
-  livechat.classList.toggle("animate-chat");
-  balao.classList.toggle("sumir-balao");
-  x.classList.toggle("chat-fechado");
-});
+  livechat.classList.toggle('animate-chat')
+  balao.classList.toggle('sumir-balao')
+  x.classList.toggle('chat-fechado')
+})
 
-texting.addEventListener('input', () => {  
-  if(texting.value.length > 0){
-  send.classList.remove("enviar-hide");
-  clip.style.display="none";
+texting.addEventListener('input', () => {
+  if (texting.value.length > 0) {
+    send.classList.remove('enviar-hide')
+    clip.style.display = 'none'
+  } else {
+    send.classList.add('enviar-hide')
+    clip.style.display = 'flex'
   }
-  else {
-  send.classList.add("enviar-hide");
-  clip.style.display="flex"; 
-  }
-});
+})
