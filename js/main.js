9d40d8cb3bffe487335e52e571ca155529ff4d4e
Renan Cesar <cesar.renan@hotmail.com>
2022-02-26 16:01:17 -0300

    Ajustando transição

--- js/main.js
+++ js/main.js
@@ -1,16 +1,24 @@
-let bubble = document.querySelector('.container-bubble')
-let balao = document.querySelector('.bubble')
-let x = document.querySelector('.x-close')
+let btOpen = document.getElementById('bt-open');
+let btClose = document.getElementById('bt-close');
+let btPai = document.getElementById('bt-pai');
 let livechat = document.querySelector('.livechat')
 let send = document.querySelector('.enviar-container')
 let texting = document.querySelector('.texting')
 let clip = document.querySelector('#clip-container')
 
-bubble.addEventListener('click', () => {
-  livechat.classList.toggle('animate-chat')
-  balao.classList.toggle('sumir-balao')
-  x.classList.toggle('chat-fechado')
+btOpen.addEventListener('click', () => {
+  livechat.classList.add('active-chat');
+  livechat.classList.remove('desactive-chat');
+  btOpen.classList.add('sumir-balao');
+  btPai.classList.remove('bt-pai');
 })
+
+btClose.addEventListener('click', () => {
+  livechat.classList.add('desactive-chat');
+  livechat.classList.remove('active-chat');
+  btOpen.classList.remove('sumir-balao');
+  btPai.classList.add('bt-pai');
+});
 
 texting.addEventListener('input', () => {
   if (texting.value.length > 0) {
@@ -20,4 +28,4 @@ texting.addEventListener('input', () => {
     send.classList.add('enviar-hide')
     clip.style.display = 'flex'
   }
-})
+});
