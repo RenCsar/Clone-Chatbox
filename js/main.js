73d0f7941c482ad73e0741fb15507eedc3c6fd7d
Renan Cesar <cesar.renan@hotmail.com>
2022-02-27 19:15:12 -0300

    Refatorando

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-let btOpen = document.getElementById('bt-open');
-let btClose = document.getElementById('bt-close');
-let btPai = document.getElementById('bt-pai');
+let btOpen = document.getElementById('bt-open')
+let btClose = document.getElementById('bt-close')
+let btPai = document.getElementById('bt-pai')
 let livechat = document.querySelector('.livechat')
 let send = document.querySelector('.enviar-container')
 let texting = document.querySelector('.texting')
 let clip = document.querySelector('#clip-container')
 
 btOpen.addEventListener('click', () => {
-  livechat.classList.add('active-chat');
-  livechat.classList.remove('desactive-chat');
-  btOpen.classList.add('sumir-balao');
-  btPai.classList.remove('bt-pai');
+  livechat.classList.add('active-chat')
+  livechat.classList.remove('desactive-chat')
+  btOpen.classList.add('sumir-balao')
+  btPai.classList.remove('bt-pai')
 })
 
 btClose.addEventListener('click', () => {
-  livechat.classList.add('desactive-chat');
-  livechat.classList.remove('active-chat');
-  btOpen.classList.remove('sumir-balao');
-  btPai.classList.add('bt-pai');
-});
+  livechat.classList.add('desactive-chat')
+  livechat.classList.remove('active-chat')
+  btOpen.classList.remove('sumir-balao')
+  btPai.classList.add('bt-pai')
+})
 
 texting.addEventListener('input', () => {
   if (texting.value.length > 0) {
@@ -28,4 +28,4 @@ texting.addEventListener('input', () => {
     send.classList.add('enviar-hide')
     clip.style.display = 'flex'
   }
-});
+})
